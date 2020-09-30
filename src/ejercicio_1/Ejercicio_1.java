c2a658f38cfb0e7752428a83788f741e6521ba78
Meliodas-R <65538839+Meliodas-R@users.noreply.github.com>
2020-09-30 11:44:41 +0200

    ·Corrección de errores:    *N/A
    ·Cambios y mejoras:
       *N/A

---
+++ src/ejercicio_1/Ejercicio_1.java
@@ -0,0 +1,13 @@
+package ejercicio_1;
+
+/**
+ * 1. Mostrar por pantalla el siguiente texto "Hola mundo.".
+ *
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+
+}
